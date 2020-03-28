edbd26096608ce2a8d9114e5f559265dcdaf1006
liuzhicheng.lzc <liuzhicheng.lzc@bytedance.com>
2020-03-28 22:02:26 +0800

    add some TODO

--- train.sh
+++ train.sh
@@ -2,7 +2,6 @@
 
 # dataset dir
 DATASET_DIR='datasets'
-# TODO:
 RAW='raw'
 MUTI_DIGIT='multi_digit'
 SINGLE_DIGIT='single_digit'
@@ -22,14 +21,19 @@ CheckCode() {
 
 # Create Train Dataset
 
+# Step1: 获取人工标注候选框
+echo "train step 1 skip, use selected figures"
+
 # Step1: 获取候选框
-python3 src/get_CNY_area.py \
-  --indir $DATASET_DIR/$RAW \
-  --outdir $DATASET_DIR/$MULTI_DIGIT \
-  > log/train_step1.log
-CheckCode $? 1
+# python3 src/get_CNY_area.py \
+#   --indir $DATASET_DIR/$RAW \
+#   --outdir $DATASET_DIR/$MULTI_DIGIT \
+#   > log/train_step1.log
+# CheckCode $? 1
 
 # Step2: 分割单字
+
+echo "errors in log/train_step2.log"
 python3 src/digit_segment.py \
   --indir $DATASET_DIR/$MUTI_DIGIT \
   --outdir $DATASET_DIR/$SINGLE_DIGIT \
